bb62063fd432a5ee943fd1fc8994274d4ea21287
Sarune IVINSKAITE <sivinska@e2r7p4.42.fr>
2019-02-04 15:21:31 +0100

    front front

--- user/register.php
+++ user/register.php
@@ -161,154 +161,57 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
  
- <!DOCTYPE html>
-<html lang="en" >
 
+
+ <!DOCTYPE html>
+<html>
 <head>
   <meta charset="UTF-8">
-  <title>camagru</title>
-  
-  
-  
-      <link rel="stylesheet" href="style.css">
-
-  
+  <title>Create an account</title>
+  <link rel="stylesheet" href="style.css">  
 </head>
 
 <body>
-
-  <html lang="en"><head>
-	<title>Create an account</title>
-	<meta charset="UTF-8">
-</head>
-<body style="background-color: #666666;">
-	
-	<div class="limits">
-		<div class="container">
-			<div class="wrap">
-				<div>
-        
-        <form class="form">
-          <span class="form-title padding">Sign Up</span>
-        <span class="form-title padding">Please fill this form to create an account.</span>
-          
-            <div class="wrap-input validate-input">
-                <label>Email</label>
-                <input class="input100" type="email" name="email">
+    <div class="container-login100" style="background-image: url('https://images.unsplash.com/photo-1513652990199-8a52e2313122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');">
+        <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
+        <span class="login100-form-title p-b-37">Create an account</span>
+            <form class="login100-form validate-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <span class="help-block"><?php echo $email_err; ?></span>
-            </div> 
+                <div class="wrap-input100 validate-input m-b-25" class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                <!--label>Email</label-->
+                <input class="input100" type="email" name="email" placeholder="Email" class="form-control" value="<?php echo $email; ?>">
+                <span class="focus-input100"></span>
+                </div>
 
-            <div class="wrap-input validate-input">
-                <label>Username</label>
-                <input class="input100" type="text" name="username">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="wrap-input validate-input">
-                <label>Password</label>
-                <input class="input100" type="password" name="password">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="wrap-input validate-input">
-                <label>Confirm Password</label>
-                <input class="input100" type="password" name="confirm_password">
+                <span class="help-block"><?php echo $username_err; ?></span> 
+                <div class="wrap-input100 validate-input m-b-25"  class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <!--label>Username</label-->
+                <input class="input100" type="text" name="username" placeholder="Username" class="form-control" value="<?php echo $username; ?>">
+                <span class="focus-input100"></span>
+                </div>
+
+                <span class="help-block"><?php echo $password_err; ?></span>   
+                <div class="wrap-input100 validate-input m-b-25" class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <!--label>Password</label-->
+                <input class="input100" type="password" name="password" placeholder="Password" class="form-control" value="<?php echo $password; ?>">
+                <span class="focus-input100"></span>
+                </div>
+
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="container-form-btn">
-              <button class="form-btn">Register</button>
- 
-            </div>
-          <div class="text-center p-t-46 p-b-20">
-						<span >
-							Already have an account? <a href="login.php">Login here</a>.
-						</span>
-					</div>
+                <div class="wrap-input100 validate-input m-b-25" class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <!--label>Confirm Password</label-->
+                <input class="input100" type="password" name="confirm_password" placeholder="Confirm password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="focus-input100"></span>
+                </div>
             
-        </form>
-    </div>    
-
-				<div class="more" style="background-image: url('https://images.unsplash.com/photo-1465161191540-aac346fcbaff?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80');">
-				</div>
-			</div>
-		</div>
-	</div>
-	
-	
-
-	
-	
-
-	
-
-	
-
-	
-	
-
-	
-
-	
-	
-
-	
-
-	
-
-
-</body></html>
-  
-  
-
-</body>
-
-</html>
-
-
-
-
-
-<!--!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-                <label>Email</label>
-                <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
-                <span class="help-block"><?php echo $email_err; ?></span>
-            </div> 
-
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-default" value="Reset">
-            </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
-        </form>
-    </div>    
+                <div class="container-login100-form-btn">
+                <button class="login100-form-btn">Sign In</button>
+                </div>
+                <div class="text-center p-t-57 p-b-20">
+                    <span class="txt1">Already have an account? <a href="login.php">Login here.</a></span>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
