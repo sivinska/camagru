19151b374d22399d6d16ac4fbc68b5b55bbbe248
Sarune Ivinskaite <33516726+sivinska@users.noreply.github.com>
2019-05-01 15:56:12 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,12 +3,11 @@
 $DB_DSN = 'mysql:host=nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com; dbname=wktjmhfmeq80m9nj';
 $DB_USER = 'r27geybatkim6x9g';
 $DB_PASSWORD = "goau3mbk301t8zqs";
-$PORT = 3306;
 
  
 /* Attempt to connect to MySQL database */
 try{
-    $pdo = new PDO($DB_DSN, $PORT, $DB_USER, $DB_PASSWORD);
+    $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
