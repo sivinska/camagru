774094ebd7e925e3b1f8b342152d33907336c7b8
Sarune Ivinskaite <33516726+sivinska@users.noreply.github.com>
2019-05-01 16:18:46 +0200

    Update setup.php

--- config/setup.php
+++ config/setup.php
@@ -1,9 +1,8 @@
 <?php
 
 include "database.php";
-$PORT = '3306';
 
-$pdo = new PDO("mysql:host=nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", $PORT, $DB_USER, $DB_PASSWORD););
+$pdo = new PDO("mysql:host=nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306", $DB_USER, $DB_PASSWORD););
 $sql = file_get_contents('camagru.sql');
 $qr = $pdo->exec($sql);
 header("location: ../index.php");
